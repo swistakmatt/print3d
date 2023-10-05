461f01a5f6f158f96b33eabdde7647f79c850084
Mateusz Świstak <38541816+swistakmatt@users.noreply.github.com>
2023-10-05 23:01:13 +0200

    Added eslint and prettier

---
+++ backend/src/index.ts
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+import mongoose from 'mongoose';
+import { Request, Response } from 'express';
+import { configureExpressMiddlewares } from './middlewares';
+
+import './controllers/auth';
+import http from 'http';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+configureExpressMiddlewares(app);
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Welcome to the eSport Drafts API!');
+});
+
+mongoose
+	.connect(process.env.MONGO_URL || 'mongodb://localhost:2137')
+	.then(async () => {
+		console.log('Connected to MongoDB');
+
+		server.listen(PORT);
+		console.log(`App is listening on port: ${PORT}`);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
