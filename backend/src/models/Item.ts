336b4f323df76f3354452c673bd0a22e57579662
Mateusz Świstak <swistakmatt@gmail.com>
2024-08-11 22:54:22 +0200

    Add Item model

--- backend/src/models/Item.ts
+++ backend/src/models/Item.ts
@@ -1,6 +1,36 @@
 import mongoose from 'mongoose';
 
-const ItemSchema = new mongoose.Schema();
+const ItemSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		description: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		category: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		image: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		files: [{ type: mongoose.Schema.Types.ObjectId, ref: 'File' }],
+		price: {
+			type: Number,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
 const Item = mongoose.model('Item', ItemSchema);
 
