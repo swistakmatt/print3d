29a5e7d5d81620e1c9d5634e4e9840b5ff2dc54a
Mateusz Świstak <swistakmatt@gmail.com>
2024-08-23 01:24:11 +0200

    Enable filtering files of a User by filename

--- backend/src/controllers/fileStorage.ts
+++ backend/src/controllers/fileStorage.ts
@@ -21,7 +21,6 @@ const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
 
 		const metadata: FileMetadata = {
 			ownerId: req.body.ownerId,
-			isPublic: toBoolean(req.body.isPublic),
 		};
 
 		const uploadedFile: any = await uploadToGridFS(req.file, metadata);
@@ -72,66 +71,38 @@ const getFilesByOwnerId = async (req: Request, res: Response) => {
 	}
 };
 
-const searchFiles = async (req: Request, res: Response) => {
+const filterUserFiles = async (req: Request, res: Response) => {
 	const client = await connectToDatabase();
 	try {
+		const { ownerId } = req.params;
+		const { query } = req.query;
+
+		if (!ownerId) {
+			return res.status(400).json({ error: { text: 'Owner ID not provided' } });
+		}
+
 		const db = client.db('print3d');
 		const bucket = new GridFSBucket(db, { bucketName: 'StorageBucket' });
 
-		const searchQuery = req.query;
+		const searchCriteria: any = { 'metadata.ownerId': ownerId };
 
-		const files = await bucket.find(searchQuery).toArray();
+		if (query) {
+			searchCriteria.$or = [
+				{ filename: { $regex: query, $options: 'i' } },
+				{ 'metadata.customField': { $regex: query, $options: 'i' } },
+			];
+		}
+
+		const files = await bucket.find(searchCriteria).toArray();
 
 		res.json(files);
 	} catch (error) {
 		console.error(error);
-		res.status(500).json({ message: 'Error fetching files' });
+		res.status(500).json({ message: 'Error fetching files', error });
 	} finally {
 		closeDatabaseConnection(client);
 	}
 };
-
-// const getFileByName = async (req: Request, res: Response) => {
-// 	const client = await connectToDatabase();
-// 	try {
-// 		const db = client.db('print3d');
-// 		const filesCollection = db.collection('StorageBucket.files');
-// 		const file = await filesCollection.findOne({
-// 			filename: req.params.filename,
-// 		});
-// 		res.json(file);
-// 	} catch (error) {
-// 		console.error(error);
-// 		res.status(500).json({ message: 'Error fetching file' });
-// 	} finally {
-// 		closeDatabaseConnection(client);
-// 	}
-// };
-
-// const getFileByFilename = async (req: Request, res: Response) => {
-// 	const client = await connectToDatabase();
-// 	try {
-// 		const db = client.db('print3d');
-// 		const bucket = new GridFSBucket(db, { bucketName: 'StorageBucket' });
-// 		const filesCollection = db.collection('StorageBucket');
-// 		const filename = req.query.filename;
-
-// 		const searchQuery = { filename: filename };
-
-// 		const files = await filesCollection.find(searchQuery).toArray();
-
-// 		if (files.length === 0) {
-// 			res.status(404).json({ message: 'File not found' });
-// 		} else {
-// 			res.json(files);
-// 		}
-// 	} catch (error) {
-// 		console.error(error);
-// 		res.status(500).json({ message: 'Error fetching file' });
-// 	} finally {
-// 		closeDatabaseConnection(client);
-// 	}
-// };
 
 const searchFileById = async (req: Request, res: Response) => {
 	const client = await connectToDatabase();
@@ -241,6 +212,7 @@ export {
 	uploadFile,
 	getAllFiles,
 	searchFileById,
+	filterUserFiles,
 	downloadFileById,
 	deleteFileById,
 	getFilesByOwnerId,
